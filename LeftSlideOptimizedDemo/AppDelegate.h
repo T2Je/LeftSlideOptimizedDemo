8ce103c4e77ffab44ca0bac94d312d685c91b180
xiaoyang <xiaoyang@xkeshi.com>
2017-04-05 23:18:43 +0800

    Initial Commit

---
+++ LeftSlideOptimizedDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LeftSlideOptimizedDemo
+//
+//  Created by 肖扬 on 2017/4/5.
+//  Copyright © 2017年 肖扬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LeftSlideOptimizedDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LeftSlideOptimizedDemo
+//
+//  Created by 肖扬 on 2017/4/5.
+//  Copyright © 2017年 肖扬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
